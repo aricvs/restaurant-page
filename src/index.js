489e655dadcefb54ff9328ccf2c66cd292e0521e
Ariel Santos <aricvs@gmail.com>
2023-12-30 22:43:30 -0300

    Remove placeholder html elements

--- src/index.js
+++ src/index.js
@@ -3,49 +3,85 @@ import { tab1 } from "./tab1.js";
 import { tab2 } from "./tab2.js";
 import { tab3 } from "./tab3.js";
 
+const addTabs = () => {
+  const documentBody = document.getElementsByTagName("BODY")[0];
+  const tabOne = document.createElement("div");
+  const tabTwo = document.createElement("div");
+  const tabThree = document.createElement("div");
+
+  tabOne.classList.add("tab1", "tab-selector", "selected");
+  tabTwo.classList.add("tab2", "tab-selector");
+  tabThree.classList.add("tab3", "tab-selector");
+
+  tabOne.textContent = "Home";
+  tabTwo.textContent = "About";
+  tabThree.textContent = "Contact";
+
+  documentBody.appendChild(tabOne);
+  documentBody.appendChild(tabTwo);
+  documentBody.appendChild(tabThree);
+};
+
+const switchTab1 = () => {
+  const firstTab = document.querySelector(".tab1");
+  const secondTab = document.querySelector(".tab2");
+  const thirdTab = document.querySelector(".tab3");
+  firstTab.addEventListener("click", () => {
+    const mainContent = document.querySelector("#content");
+    while (mainContent.firstChild) {
+      mainContent.removeChild(mainContent.firstChild);
+    }
+
+    firstTab.classList.add("selected");
+    secondTab.classList.remove("selected");
+    thirdTab.classList.remove("selected");
+
+    tab1();
+  });
+};
+
+const switchTab2 = () => {
+  const firstTab = document.querySelector(".tab1");
+  const secondTab = document.querySelector(".tab2");
+  const thirdTab = document.querySelector(".tab3");
+  secondTab.addEventListener("click", () => {
+    const mainContent = document.querySelector("#content");
+    while (mainContent.firstChild) {
+      mainContent.removeChild(mainContent.firstChild);
+    }
+
+    secondTab.classList.add("selected");
+    firstTab.classList.remove("selected");
+    thirdTab.classList.remove("selected");
+
+    tab2();
+  });
+};
+
+const switchTab3 = () => {
+  const firstTab = document.querySelector(".tab1");
+  const secondTab = document.querySelector(".tab2");
+  const thirdTab = document.querySelector(".tab3");
+  thirdTab.addEventListener("click", () => {
+    const mainContent = document.querySelector("#content");
+    while (mainContent.firstChild) {
+      mainContent.removeChild(mainContent.firstChild);
+    }
+
+    thirdTab.classList.add("selected");
+    firstTab.classList.remove("selected");
+    secondTab.classList.remove("selected");
+
+    tab3();
+  });
+};
+
+const tabSwitcher = () => {
+  switchTab1();
+  switchTab2();
+  switchTab3();
+};
+
+addTabs();
 tab1();
-
-// remove tabs from html and add them here
-
-const firstTab = document.querySelector(".tab1");
-const secondTab = document.querySelector(".tab2");
-const thirdTab = document.querySelector(".tab3");
-
-firstTab.addEventListener("click", () => {
-  const mainContent = document.querySelector("#content");
-  while (mainContent.firstChild) {
-    mainContent.removeChild(mainContent.firstChild);
-  }
-
-  firstTab.classList.add("selected");
-  secondTab.classList.remove("selected");
-  thirdTab.classList.remove("selected");
-
-  tab1();
-});
-
-secondTab.addEventListener("click", () => {
-  const mainContent = document.querySelector("#content");
-  while (mainContent.firstChild) {
-    mainContent.removeChild(mainContent.firstChild);
-  }
-
-  secondTab.classList.add("selected");
-  firstTab.classList.remove("selected");
-  thirdTab.classList.remove("selected");
-
-  tab2();
-});
-
-thirdTab.addEventListener("click", () => {
-  const mainContent = document.querySelector("#content");
-  while (mainContent.firstChild) {
-    mainContent.removeChild(mainContent.firstChild);
-  }
-
-  thirdTab.classList.add("selected");
-  firstTab.classList.remove("selected");
-  secondTab.classList.remove("selected");
-
-  tab3();
-});
+tabSwitcher();
